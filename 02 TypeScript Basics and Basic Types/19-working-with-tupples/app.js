bd2a9717f0b5e849fa0603a033ff1c2016edcb07
Saddam Husein <saddambinhusein2@gmail.com>
2023-11-17 21:52:56 +0800

    Section 2: TypeScript Basics and Basic Types (19 Working with tupples)

--- 02 TypeScript Basics and Basic Types/19-working-with-tupples/app.js
+++ 02 TypeScript Basics and Basic Types/19-working-with-tupples/app.js
@@ -1,23 +1,18 @@
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
 var person = {
+    // const person = {
     name: 'Saddam',
     age: 30,
     hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author'],
 };
+person.role.push('admin'); // work with no error when we append new elements into the role tupple, even we didn't define the tupple type yet. Push actually is an exception which is allowed in tuples. So unfortunately TypeScript can't catch this error,
+// person.role[1] = 10; // error. person.role[1] should assign to string type
+person.role = [0, 'admin', 'user']; // empty array or extra elementy would not allowed (get an error). should always consistent with the tupple type we have defined
 // console.log(person);
-// console.log(person.name);
-/* Typescript inference for array
-let favoriteActivities: string[]; // any[] for mix array (e.g. ['Sports', 1]). number[] for number array (e.g. [1, 2, 3])
-favoriteActivities = 'Sprots'; // we get an error in typescript that 'Sports' not assignable to array of string
-favoriteActivities = ['Sport']; // correctly
-favoriteActivities = ['Sport', 1]; // in mix array, we should change string[] into any[]
- */
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    // console.log(hobby);
-    // console.log(hobby.toUpperCase()); // we wouldn't get any complain in typescript when we using toUpperCase() method
-    console.log(hobby.map()); // we will get an error when we using .map() method, because map() is available on array not on string array (string[])
-}
+console.log(person.role);
+/*
+  notes:
+  you might want to consider a tuple instead of an array to get even more strictness
+  into your app to be even clearer about the type of data you're working with and the type
+  of data you're expecting.
+*/
--- 02 TypeScript Basics and Basic Types/19-working-with-tupples/app.ts
+++ 02 TypeScript Basics and Basic Types/19-working-with-tupples/app.ts
@@ -1,25 +1,26 @@
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string]; // define role to tupple type with number and string rules
+} = {
+  // const person = {
   name: 'Saddam',
   age: 30,
   hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author'],
 };
 
+person.role.push('admin'); // work with no error when we append new elements into the role tupple, even we didn't define the tupple type yet. Push actually is an exception which is allowed in tuples. So unfortunately TypeScript can't catch this error,
+// person.role[1] = 10; // error. person.role[1] should assign to string type
+person.role = [0, 'admin', 'user']; // empty array or extra elementy would not allowed (get an error). should always consistent with the tupple type we have defined
+
 // console.log(person);
-// console.log(person.name);
+console.log(person.role);
 
-/* Typescript inference for array
-let favoriteActivities: string[]; // any[] for mix array (e.g. ['Sports', 1]). number[] for number array (e.g. [1, 2, 3])
-favoriteActivities = 'Sprots'; // we get an error in typescript that 'Sports' not assignable to array of string
-favoriteActivities = ['Sport']; // correctly
-favoriteActivities = ['Sport', 1]; // in mix array, we should change string[] into any[]
- */
-
-for (const hobby of person.hobbies) {
-  // console.log(hobby);
-  // console.log(hobby.toUpperCase()); // we wouldn't get any complain in typescript when we using toUpperCase() method
-  console.log(hobby.map()); // we will get an error when we using .map() method, because map() is available on array not on string array (string[])
-}
+/*
+  notes:
+  you might want to consider a tuple instead of an array to get even more strictness 
+  into your app to be even clearer about the type of data you're working with and the type 
+  of data you're expecting.
+*/
