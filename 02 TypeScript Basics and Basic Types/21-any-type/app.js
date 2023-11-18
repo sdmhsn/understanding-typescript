e21c0cc494795d64951f32da04edfaf97b17e6ee
Saddam Husein <saddambinhusein2@gmail.com>
2023-11-18 18:21:27 +0800

    Section 2: TypeScript Basics and Basic Types (21 Any type)

---
+++ 02 TypeScript Basics and Basic Types/21-any-type/app.js
@@ -0,0 +1,18 @@
+var person = {
+    // const person = {
+    name: 'Saddam',
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+};
+console.log(person);
+var favoriteActivities;
+favoriteActivities = ['Sports', 1, 2];
+/*
+  notes:
+  The any type is the most flexible type you can assign in TypeScript. This type doesn't tell TypeScript anything.
+  It basically means you can store any kind of value in there. So any is really flexible.
+  And this can sound great at fist, but actually it's a big disadvantage and you absolutely want to avoid any whenever
+  possible. Because any takes away basically all advantages TypeScript gives you. It gives you the same experience
+  you have with vanilla JavaScript where you also have to any type on everything so to say. Any basically makes sure that
+  the TypeScript compiler can't check anything,
+*/
