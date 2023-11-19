86fad64a817b1eabb72e4276958b2bbeb9bef1df
Saddam Husein <saddambinhusein2@gmail.com>
2023-11-19 12:17:43 +0800

    Section 2: TypeScript Basics and Basic Types (23 Literal types)

---
+++ 02 TypeScript Basics and Basic Types/23-literal-types/app.js
@@ -0,0 +1,28 @@
+function combine(input1, input2, resultConversion // literal type combined with union type
+) {
+    var result;
+    if ((typeof input1 === 'number' && typeof input2 === 'number') ||
+        resultConversion === 'as-number') {
+        result = +input1 + +input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+    // if (resultConversion === 'as-number') {
+    //   return +result;
+    // } else {
+    //   return result.toString();
+    // }
+}
+var combinedAges = combine(30, 26, 'as-number');
+console.log(combinedAges);
+var combinedStringAges = combine('30', '26', 'as-number');
+console.log(combinedStringAges);
+var combinedNames = combine('Saddam', 'Rahmat', 'as-text');
+console.log(combinedNames);
+/*
+  Notes:
+  Literal types are types where you don't just say that a certain variable or a parameter should hold, let's say a
+  number or a string, but where you are very clear about the exact value it should hold.
+*/
