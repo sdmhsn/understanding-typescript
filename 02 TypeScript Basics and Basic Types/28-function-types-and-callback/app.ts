79117b0910e10ebb45bcbfbeb489a855d6827448
Saddam Husein <saddambinhusein2@gmail.com>
2023-11-20 21:47:59 +0800

    Section 2: TypeScript Basics and Basic Types (28 Function types and callback)

---
+++ 02 TypeScript Basics and Basic Types/28-function-types-and-callback/app.ts
@@ -0,0 +1,21 @@
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
+addAndHandle(10, 20, (result) => {
+  // anonymous function as addAndHandle parameter
+  /*  without explicitly stating the type here (result) because TypeScript knows the result will be a number 
+      because we made it really clear that the callback will get one argument which is a number [cb: (num: number) => void]
+  */
+  /*  without explicitly stating the type here because TypeScript knows the result will be a number because 
+      we made it really clear that the callback will get one argument which is a number.
+  */
+  console.log(result);
+  // return result; // it shouldn't return anything. This however, is not a mistake or a bug in TypeScript. It actually happens on purpose. By specifying void here our callback type, we're essentially saying we'll ignore any results you might be returning here.
+});
+
+/*
+  Notes:
+  callbacks and function types
+*/
