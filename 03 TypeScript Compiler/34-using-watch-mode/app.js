69ffa876e1f79a70723efa23b3526417f84b0856
Saddam Husein <saddambinhusein2@gmail.com>
2023-11-21 08:20:37 +0800

    Section 3: TypeScript Compiler (34 Using watch mode)

---
+++ 03 TypeScript Compiler/34-using-watch-mode/app.js
@@ -0,0 +1,10 @@
+var userName = 'Saddam';
+userName = 5;
+console.log(userName);
+/*
+  Notes:
+  - watch mode command
+    tsc app.ts --watch
+    or
+    tsc app.ts -w
+*/
---
+++ 03 TypeScript Compiler/34-using-watch-mode/app.ts
@@ -0,0 +1,11 @@
+const userName = 'Saddam';
+userName = 5;
+console.log(userName);
+
+/*
+  Notes:
+  - watch mode command
+    tsc app.ts --watch
+    or
+    tsc app.ts -w
+*/
