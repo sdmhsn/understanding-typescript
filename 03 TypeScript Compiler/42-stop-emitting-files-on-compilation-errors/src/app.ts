73225b8c3843775ed97ae9bcea457ed9a1da7993
Saddam Husein <saddambinhusein2@gmail.com>
2023-11-23 01:04:12 +0800

    Section 3: TypeScript Compiler (42 Stop emitting files on compilation errors)

--- 03 TypeScript Compiler/42-stop-emitting-files-on-compilation-errors/src/app.ts
+++ 03 TypeScript Compiler/42-stop-emitting-files-on-compilation-errors/src/app.ts
@@ -1,6 +1,13 @@
-const button = document.querySelector('button')!;
+// const button = document.querySelector('button')!;
+const button = document.querySelector('button');
 
 // this comment will be remove in app.js
 button.addEventListener('click', () => {
+  // we created error by removing ! sign in
   console.log('Clicked!');
 });
+
+/*
+  Notes:
+  If we set "noEmitOnError": true in tsconfig.json file, that problematic files (app.js) will not be generated.
+*/
