bdafd5023e63e8ef7c48dd083fe7a3dc47aeb56a
Saddam Husein <saddambinhusein2@gmail.com>
2023-11-23 13:42:20 +0800

    Section 5: Classes and Interfaces (60 Compiling to javascript)

--- 05 Classes and Interfaces/60-compiling-to-javascript/dist/app.js
+++ 05 Classes and Interfaces/60-compiling-to-javascript/dist/app.js
@@ -1,8 +1,13 @@
 "use strict";
 class Department {
+    /*
+      Notes: the name field above (name: string;) which we have does not exist in app.js in the class. This would actually
+      be supported in modern JavaScript but it depends on the version of JavaScript we're building for.
+      So in ES6 alone, it's not supported actually
+    */
     constructor(n) {
         this.name = n;
     }
 }
-const accounting = new Department('Accounting'); // object. object value = 'Accounting
+const accounting = new Department('Accounting');
 console.log(accounting);
--- 05 Classes and Interfaces/60-compiling-to-javascript/src/app.ts
+++ 05 Classes and Interfaces/60-compiling-to-javascript/src/app.ts
@@ -1,10 +1,16 @@
 class Department {
   name: string; // property
 
+  /*
+    Notes: the name field above (name: string;) which we have does not exist in app.js in the class. This would actually 
+    be supported in modern JavaScript but it depends on the version of JavaScript we're building for. 
+    So in ES6 alone, it's not supported actually
+  */
+
   constructor(n: string) {
     this.name = n;
   }
 }
 
-const accounting = new Department('Accounting'); // object. object value = 'Accounting
+const accounting = new Department('Accounting');
 console.log(accounting);
