b2f3c6b0a89a1184dcd619214d1413c9a9a33e95
Saddam Husein <saddambinhusein2@gmail.com>
2023-11-23 12:10:56 +0800

    Section 4: Next Generation TypeScript and JavaScript (54 Array and object destructuring)

---
+++ 04 Next Generation TypeScript and JavaScript/54-array-object-destructuring/src/app.ts
@@ -0,0 +1,13 @@
+/* Sample 1: */
+const hobbies = ['Sports', 'Cooking'];
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+console.log(hobbies, hobby1, hobby2);
+
+/* Sample 2: */
+const person = {
+  firstName: 'Saddam',
+  age: 30,
+};
+
+const { firstName: userName, age } = person; // firstName: userName is not a type assignment here. This is JavaScript syntax, where we alias rename a property we're pulling out of person.
+console.log(userName, age, person);
