c7aeaba959ed7af9ebdd5eb5b8d01e6efffee853
Saddam Husein <saddambinhusein2@gmail.com>
2023-11-23 10:14:24 +0800

    Section 3: TypeScript Compiler (44 Code quality options)

--- 03 TypeScript Compiler/44-code-quality-options/dist/app.js
+++ 03 TypeScript Compiler/44-code-quality-options/dist/app.js
@@ -1,21 +1,16 @@
 "use strict";
-// const button = document.querySelector('button')!;
-// const button = document.querySelector('button');
-// button.addEventListener('click', () => {
-//   console.log('Clicked!');
-// });
-/*
-  Notes:  remove exclamation mark (!) in  const button = document.querySelector('button')!;  will not get any typescript error
-          when set the "strictNullChecks" to false in tsconfig.json
-*/
 const button = document.querySelector('button');
-if (button) {
-    button.addEventListener('click', () => {
-        console.log('Clicked!');
-    });
+// let userName = 'Saddam'; // global variable
+function clickHandler(message) {
+    let userName = 'Saddam'; // local variable.  "noUnusedLocals": true
+    /*
+      You see, this has yellow squiggly lines because it's not an error, really.
+      It's more like a warning or a hint. So if I compile here, you'll see now, however, I do get an error
+      because TypeScript only knows errors. And there we see that the username is declared, but its value is
+      never read because we made sure that we don't want unused local variables.
+    */
+    console.log('Clicked!' + message);
 }
-/*
-  Notes: Now even without the exclamation mark (!), and strictNullChecks to true mode we get no error
-  because TypeScript understands that this code is inside of this if statement and that this if statement
-  makes sure that button is not null and that this will not fail.
-*/
+if (button) {
+    button.addEventListener('click', clickHandler.bind(null, 'You are welcome'));
+}
--- 03 TypeScript Compiler/44-code-quality-options/src/app.ts
+++ 03 TypeScript Compiler/44-code-quality-options/src/app.ts
@@ -1,25 +1,19 @@
-// const button = document.querySelector('button')!;
-// const button = document.querySelector('button');
+const button = document.querySelector('button')!;
 
-// button.addEventListener('click', () => {
-//   console.log('Clicked!');
-// });
+// let userName = 'Saddam'; // global variable
 
-/*
-  Notes:  remove exclamation mark (!) in  const button = document.querySelector('button')!;  will not get any typescript error
-          when set the "strictNullChecks" to false in tsconfig.json
-*/
+function clickHandler(message: string) {
+  let userName = 'Saddam'; // local variable.  "noUnusedLocals": true
 
-const button = document.querySelector('button');
-
-if (button) {
-  button.addEventListener('click', () => {
-    console.log('Clicked!');
-  });
+  /*
+    You see, this has yellow squiggly lines because it's not an error, really. 
+    It's more like a warning or a hint. So if I compile here, you'll see now, however, I do get an error
+    because TypeScript only knows errors. And there we see that the username is declared, but its value is 
+    never read because we made sure that we don't want unused local variables.
+  */
+  console.log('Clicked!' + message);
 }
 
-/*
-  Notes: Now even without the exclamation mark (!), and strictNullChecks to true mode we get no error
-  because TypeScript understands that this code is inside of this if statement and that this if statement 
-  makes sure that button is not null and that this will not fail.
-*/
+if (button) {
+  button.addEventListener('click', clickHandler.bind(null, 'You are welcome'));
+}
