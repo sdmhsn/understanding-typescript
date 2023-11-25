50a086a064eb7bf1366da04bb7633d811516e821
Saddam Husein <saddambinhusein2@gmail.com>
2023-11-25 17:06:38 +0800

    Section 5: Classes and Interfaces (62 Private and public access modifiers: project setup)

---
+++ 05 Classes and Interfaces/62-private-and-public-access-modifiers/dist/app.js
@@ -0,0 +1,12 @@
+"use strict";
+class Department {
+    constructor(n) {
+        this.name = n;
+    }
+    describe() {
+        console.log('Department: ' + this.name);
+    }
+}
+const accounting = new Department('Accounting');
+console.log(accounting);
+accounting.describe();
---
+++ 05 Classes and Interfaces/62-private-and-public-access-modifiers/src/app.ts
@@ -0,0 +1,15 @@
+class Department {
+  name: string;
+
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log('Department: ' + this.name);
+  }
+}
+
+const accounting = new Department('Accounting');
+console.log(accounting);
+accounting.describe();
