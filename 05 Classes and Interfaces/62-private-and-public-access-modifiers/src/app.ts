5c23103d18ec5ed2c745167cae9ac9dc8c18ed2d
Saddam Husein <saddambinhusein2@gmail.com>
2023-11-26 13:06:36 +0800

    Section 5: Classes and Interfaces (62 Private and public access modifiers: public property)

--- 05 Classes and Interfaces/62-private-and-public-access-modifiers/src/app.ts
+++ 05 Classes and Interfaces/62-private-and-public-access-modifiers/src/app.ts
@@ -1,5 +1,5 @@
 class Department {
-  name: string;
+  public name: string; // public property are accessible from outside. we also don't need to add public keyword because it is exactly the same as name with public in front of it.
   private employees: string[] = []; // store data employee. private means employees is now a property which is only accessible from inside the class
 
   constructor(n: string) {
@@ -27,8 +27,24 @@ accounting.describe();
 accounting.addEmployee('Saddam');
 accounting.addEmployee('Rahmat');
 
-console.log(accounting.employees); // we can't access the private property (employess) anymore
-accounting.employees[1] = 'Fuad'; // we can't modify the private property (employess) anymore
+// console.log(accounting.employees); // we can't access the private property (employess) anymore
+// accounting.employees[1] = 'Fuad'; // we can't modify the private property (employess) anymore
+
+accounting.name = 'NEW NAME';
 
 console.log(accounting);
 accounting.printEmployeeInformation();
+
+/*
+  Notes:
+  - TypeScript introduces this feature, which of course does not work during runtime however, because JavaScript 
+    until recently only knew public properties. So this line here, frozen error during TypeScript compilation.
+    If we compile it to JavaScript nonetheless, this code will still execute at runtime without errors
+    because again, JavaScript on its own, until recently didn't know this concept.
+
+  - JavaScript doesn't know public and private. Only in very modern versions added recently such a thing exists.
+    In the past, JavaScript didn't know private or public properties.
+
+  - And therefore, TypeScript only supports this because it checks it during compilation, not at runtime. With it 
+    however, we can avoid code like this and force everyone to write cleaner code.
+*/
