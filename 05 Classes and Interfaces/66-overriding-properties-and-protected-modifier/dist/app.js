bcc913e6e5440a2fbbcabbc4e2df81719e07f296
Saddam Husein <saddambinhusein2@gmail.com>
2023-11-27 13:42:22 +0800

    Section 5: Classes and Interfaces (66 Overriding properties and protected modifier: project setup)

---
+++ 05 Classes and Interfaces/66-overriding-properties-and-protected-modifier/dist/app.js
@@ -0,0 +1,57 @@
+"use strict";
+class Department {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        this.employees = [];
+        // we don't have to initialize value of field in constructor
+        // this.id = id;
+        // this.name = n;
+    }
+    describe() {
+        console.log(`Department (${this.id}):  ${this.name}`);
+    }
+    addEmployee(employee) {
+        // this.id = 'd2'; // typescript error. we can't modify the id readonly value, even inside the class. (e.g. constructor(private readonly id: string...))
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
+}
+class ITDepartment extends Department {
+    constructor(id, admins) {
+        super(id, 'IT'); // super() calls the constructor of the base class (Department). id from constructor ITDepartment passed to this super(). 'IT' is hard code a value for the name of Department class
+        this.admins = admins; // un-shorthand initialization. when we want using the shorthand initialization, we should add private/public keyword before admins: string[] in ITDepartment constructor
+    }
+}
+class AccountingDepartment extends Department {
+    constructor(id, reports) {
+        super(id, 'Accounting');
+        this.reports = reports;
+    }
+    addReports(text) {
+        this.reports.push(text);
+    }
+    printReports() {
+        console.log(this.reports);
+    }
+}
+const it = new ITDepartment('dd1', ['Saddam']); // instantiate id and admins to ITDepartment (constructor(id: string, admins: string[]))
+it.name = 'NEW NAME'; // change the ITDepartment property name, through the base class (Department) property name
+it.describe(); // we can also use the base class (Department) method through the ITDepartment class
+it.addEmployee('Rahmat');
+it.addEmployee('Fuad');
+it.printEmployeeInformation();
+console.log(it);
+const accounting = new AccountingDepartment('dd2', []);
+accounting.addReports('Something went wrong...');
+accounting.printReports();
+console.log(accounting);
+/*
+  Note:
+  - We can only inherit from one class (e.g. class ITDepartment extends Department), so we can't inherit from multiple classes
+  - super() calls the constructor of the base class, so department constructor in this case
+  - if we are using something that uses the "this" keyword in constructor, we have to do that after calling super
+*/
