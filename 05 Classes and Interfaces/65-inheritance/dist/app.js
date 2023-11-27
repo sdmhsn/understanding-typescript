004101d4260dbfa532848ac43b8326adfa5248c5
Saddam Husein <saddambinhusein2@gmail.com>
2023-11-27 11:02:07 +0800

    Section 5: Classes and Interfaces (65 inheritance: project setup)

---
+++ 05 Classes and Interfaces/65-inheritance/dist/app.js
@@ -0,0 +1,32 @@
+"use strict";
+class Department {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        this.employees = [];
+        // we don't have to initialize value of field in constructor
+        // this.id = id;
+        // this.name = n;
+    }
+    describe() {
+        console.log(`Department (${this.id}):  ${this.name}`);
+    }
+    addEmployee(employee) {
+        // this.id = 'd2'; // typescript error. we can't modify the id value, even inside the class. (e.g. constructor(private readonly id: string...))
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
+}
+const accounting = new Department('d1', 'Accounting'); // instantiate id, name to accounting object
+accounting.describe();
+accounting.addEmployee('Saddam');
+accounting.addEmployee('Rahmat');
+console.log(accounting);
+accounting.printEmployeeInformation();
+/*
+  Note:
+  -
+*/
