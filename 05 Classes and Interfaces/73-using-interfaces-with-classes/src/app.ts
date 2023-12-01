ed5de32e020ad78acecc66e7784e666512e8f185
Saddam Husein <saddambinhusein2@gmail.com>
2023-12-01 14:46:14 +0800

    Section 5: Classes and Interfaces (73 Using interfaces with classes)

--- 05 Classes and Interfaces/73-using-interfaces-with-classes/src/app.ts
+++ 05 Classes and Interfaces/73-using-interfaces-with-classes/src/app.ts
@@ -1,30 +1,58 @@
-interface Person {
-  // name: string = 'Saddam'; // we don't have this. and if we were to try and assign a default value here, we would get an error that an interface cannot have an initializer.
+interface Greetable {
   name: string;
   age: number;
 
   greet(phrase: string): void;
 }
 
-let user1: Person; // we can use our interface as a type.
+class Person implements Greetable {
+  name: string;
+  age = 30; // age value inside
 
-user1 = {
-  name: 'Saddam',
-  age: 30,
+  constructor(n: string) {
+    this.name = n;
+  }
 
   greet(phrase: string) {
     console.log(`${phrase} ${this.name}`);
-  },
-};
+  }
+}
 
+let user1: Greetable; // we can use our interface as a type.
+
+user1 = new Person('Saddam'); // instantiate name on user1 object using Person class
 user1.greet('Hello there - I am');
+console.log(user1); // we see that it is a normal object with the age and the name in this case, but the key takeaway is that it is based on a class which implements the interface.
+/*
+  console.log(user1) output:
+  {
+    "age": 30,
+    "name": "Saddam"
+  }
+*/
+console.log(Person);
+/*
+  console.log(Person) output:
+  class Person {
+    constructor(n) {
+        this.age = 30; // age value inside
+        this.name = n;
+    }
+    greet(phrase) {
+        console.log(`${phrase} ${this.name}`);
+    }
+  }
+*/
 
 /*
   Note:
-  - an interface describes the structure of an object. we can use it to describe how an object should look like.
-  - start with a capital character (e.g. Person), not a must do but a recommendation and a convention.
-  - what can we now do with this person interface? what's the idea behind it? well, we can now use this to type check 
-    an object, for example.
-  - an interface allows us to define the structure of an object. we can use it as a type to type check for objects 
-    that must have this structure
+  - an interface and a custom type are not exactly the same whilst often, you can use them interchangeably 
+    and we can use an interface instead of a custom type or the other way around.
+  - one nature difference is that interfaces can only be used to describe the structure of an object, we 
+    can use type for that as well, but instead of a custom type, we can also store other things like union 
+    types and so on what we did early in the course. that sounds like type is therefore more flexible, but the 
+    other side of the coin is that interface is clearer. when we define something as an interface, it's super 
+    clear that you want to define the structure of an object with that.
+  - another thing we can do with interfaces but we would only be able to do with custom types is we can 
+    implement an interface in a class.
 */
