cb360a0131f249db36953f36a0a2c85b5c5727a1
Saddam Husein <saddambinhusein2@gmail.com>
2023-12-02 11:28:21 +0800

    Section 5: Classes and Interfaces (75 readonly interface properties: project setup)

---
+++ 05 Classes and Interfaces/75-readonly-interface-properties/dist/app.js
@@ -0,0 +1,18 @@
+"use strict";
+class Person {
+    constructor(n) {
+        this.age = 30; // age value inside
+        this.name = n;
+    }
+    greet(phrase) {
+        console.log(`${phrase} ${this.name}`);
+    }
+}
+let user1; // we can use our interface as a type.
+user1 = new Person('Saddam');
+user1.greet('Hello there - I am');
+console.log(user1);
+/*
+  Note:
+  -
+*/
---
+++ 05 Classes and Interfaces/75-readonly-interface-properties/src/app.ts
@@ -0,0 +1,30 @@
+interface Greetable {
+  name: string;
+  age: number;
+
+  greet(phrase: string): void;
+}
+
+class Person implements Greetable {
+  name: string;
+  age = 30; // age value inside
+
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  greet(phrase: string) {
+    console.log(`${phrase} ${this.name}`);
+  }
+}
+
+let user1: Greetable; // we can use our interface as a type.
+
+user1 = new Person('Saddam');
+user1.greet('Hello there - I am');
+console.log(user1);
+
+/*
+  Note:
+  - 
+*/
