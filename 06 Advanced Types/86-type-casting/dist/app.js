c516b1c1ae2c9ff6599d0b03991886142cb67f98
Saddam Husein <saddambinhusein2@gmail.com>
2023-12-06 21:23:38 +0800

    Section 6: Advanced Types (86 Type casting)

--- 06 Advanced Types/86-type-casting/dist/app.js
+++ 06 Advanced Types/86-type-casting/dist/app.js
@@ -1,33 +1,42 @@
 "use strict";
-function moveAnimal(animal) {
-    let speed;
-    // discriminated union check
-    switch (animal.type) {
-        case 'bird':
-            speed = animal.flyingSpeed;
-            break;
-        case 'horse':
-            speed = animal.runningSpeed;
-            break;
-        default:
-            break;
-    }
-    console.log(`Moving with speed: ${speed}`);
-}
-moveAnimal({ type: 'bird', flyingSpeed: 400 });
-moveAnimal({ type: 'horse', runningSpeed: 800 });
+/*  Sample 1:
+// const paragraph = document.querySelector('p'); // HTMLParagraphElement. when we hover this const, TypeScript actually finds out that this is a paragraph element.
+const paragraph = document.getElementById('message-output'); // HTMLElement. TypeScript detect this as HTMLElement, it doesn't know which specific HTML element that is.
+*/
+// Sample 2 (error):
+// const userInputElement = document.getElementById('user-input'); // HTMLElement. we as a developer, we know that here, we reach out to this input element, but TypeScript doesn't know that. TypeScript doesn't read our HTML code, so TypeScript only knows that it's some HTML element.
+// userInputElement.value = 'Hi there!'; // if we would want to set the value property of this therefore, and it would try to do userInputElement.value equal "Hi There!", you see that we actually get an error. We get an error because for one, this object is possibly null and Property 'value' does not exist on type 'HTMLElement'.
+// Sample 3 (how to fix error):
+// const userInputElement = document.getElementById('user-input')!;
+// userInputElement.value = 'Hi there!'; // we can fix 'userInputElement' is possibly 'null to not null' by adding an exclamation mark (!) after document.getElementById('user-input'), but we still get an error that 'value' does not exist on type HTML Element because this generic type, which basically any HTML element has as a type, does not support properties that are specific to, well, specific HTML elements.
+// So, we would need to tell TypeScript that actually what we select here is not just not null, but that it's also of type HTMLInputElement by these two ways:
+// version 1:
+// const userInputElement = <HTMLInputElement>(
+//   document.getElementById('user-input')!
+// ); // We add angled brackets opening and closing <> and then between these brackets, the type of the thing after the angled brackets we add HTMLInputElement. And this type is globally available because in our tsconfig.json file, we are including the dom lib. With that, TypeScript knows that whatever we select here after the angled brackets will be of type HTMLInputElement, and therefore it doesn't complain anymore.
+// userInputElement.value = 'Hello there!';
+// version 2:
+const userInputElement = document.getElementById('user-input'); // we can also add something after the thing we want to type cast, so after this selection in this case, and that thing is the "as" keyword. And then here we again add the type to which we wanna cast this (HTMLInputElement). So, this now tells TypeScript that the expression in front of it, in this case, this expression here, will yield a value of type HTMLInputElement. Therefore, again, we get no error.
+userInputElement.value = 'Hello there!';
+// other version (if we're not sure that this (userInputElement) will not return null / alternative to using the exclamation mark (!), we can use an if check statement):
+// const userInputElement = document.getElementById(
+//   'user-input'
+// ) as HTMLInputElement; // casting as HTMLInputElement defines outside the if statement
+// if (userInputElement) {
+//   userInputElement.value = 'Hello there!';
+// }
+// or
+// const userInputElement = document.getElementById('user-input');
+// if (userInputElement) {
+//   (userInputElement as HTMLInputElement).value = 'Hello there!'; // casting as HTMLInputElement defines inside the if statement (userInputElement as HTMLInputElement))
+// }
 /*
   Notes:
-  - discriminated union is a pattern, which we can use when working with union types, that makes implementing type guards easier.
-    it is available when we work with object types.
-  - we can use in and instanceof to check if flyingSpeed is in animal, but animal instanceof bird will not work because data is an interface,
-    which is not compiled to JavaScript.
-  - now this is a discriminated union because we have one common property in every object that makes up our union, which describes that object,
-    so that we can use this property that describes this object in our check to have 100% type safety and understand which properties are
-    available for such an object and which properties are not.
-  - so instead of checking for the existence of a given property, or instead of using instanceof, we use a property we know that exists
-    to check which type of objects we're working with.
-  - we also eliminate the danger of mistyping because TypeScript understands that the only cases we can have here, for animal type, are bird
-    and horse and hence it gives us this art of completion and if you would introduce a typo, we would immediately get an error.
-  - so therefore this is a very useful pattern when working with objects and union types.
+  - Type casting helps we tell TypeScript that some value is of a specific type where TypeScript is
+    not able to detect it on it's own, but we as a developer know that it will be the case.
+  - TypeScript does not dive into our HTML files and analyze them.
+  - The exclamation mark (!) allows us to tell TypeScript that the expression in front of it (e.g. document.getElementById('user-input')) will never yield null.
+    If we know as developer that this (userInputElement) will never return null, then we can use this exclamation mark.
+  - If we're not sure that this (userInputElement) will not return null, we can use an if check.
+  - Type Casting is called Type Assertions in the official Docs and it's explained here: https://www.typescriptlang.org/docs/handbook/2/everyday-types.html#type-assertions
 */
--- 06 Advanced Types/86-type-casting/src/app.ts
+++ 06 Advanced Types/86-type-casting/src/app.ts
@@ -1,37 +1,53 @@
-interface Bird {
-  type: 'bird'; // discriminated union common property
-  flyingSpeed: number;
-}
+/*  Sample 1:
+// const paragraph = document.querySelector('p'); // HTMLParagraphElement. when we hover this const, TypeScript actually finds out that this is a paragraph element.
+const paragraph = document.getElementById('message-output'); // HTMLElement. TypeScript detect this as HTMLElement, it doesn't know which specific HTML element that is.
+*/
 
-interface Horse {
-  type: 'horse'; // discriminated union common property
-  runningSpeed: number;
-}
+// Sample 2 (error):
+// const userInputElement = document.getElementById('user-input'); // HTMLElement. we as a developer, we know that here, we reach out to this input element, but TypeScript doesn't know that. TypeScript doesn't read our HTML code, so TypeScript only knows that it's some HTML element.
+// userInputElement.value = 'Hi there!'; // if we would want to set the value property of this therefore, and it would try to do userInputElement.value equal "Hi There!", you see that we actually get an error. We get an error because for one, this object is possibly null and Property 'value' does not exist on type 'HTMLElement'.
 
-type Animal = Bird | Horse;
+// Sample 3 (how to fix error):
+// const userInputElement = document.getElementById('user-input')!;
+// userInputElement.value = 'Hi there!'; // we can fix 'userInputElement' is possibly 'null to not null' by adding an exclamation mark (!) after document.getElementById('user-input'), but we still get an error that 'value' does not exist on type HTML Element because this generic type, which basically any HTML element has as a type, does not support properties that are specific to, well, specific HTML elements.
 
-function moveAnimal(animal: Animal) {
-  let speed;
+// So, we would need to tell TypeScript that actually what we select here is not just not null, but that it's also of type HTMLInputElement by these two ways:
+// version 1:
+// const userInputElement = <HTMLInputElement>(
+//   document.getElementById('user-input')!
+// ); // We add angled brackets opening and closing <> and then between these brackets, the type of the thing after the angled brackets we add HTMLInputElement. And this type is globally available because in our tsconfig.json file, we are including the dom lib. With that, TypeScript knows that whatever we select here after the angled brackets will be of type HTMLInputElement, and therefore it doesn't complain anymore.
+// userInputElement.value = 'Hello there!';
 
-  // discriminated union check
-  switch (animal.type) {
-    case 'bird':
-      speed = animal.flyingSpeed;
-      break;
-    case 'horse':
-      speed = animal.runningSpeed;
-      break;
-    default:
-      break;
-  }
+// version 2:
+const userInputElement = document.getElementById(
+  'user-input'
+) as HTMLInputElement; // we can also add something after the thing we want to type cast, so after this selection in this case, and that thing is the "as" keyword. And then here we again add the type to which we wanna cast this (HTMLInputElement). So, this now tells TypeScript that the expression in front of it, in this case, this expression here, will yield a value of type HTMLInputElement. Therefore, again, we get no error.
+userInputElement.value = 'Hello there!';
 
-  console.log(`Moving with speed: ${speed}`);
-}
+// other version (if we're not sure that this (userInputElement) will not return null / alternative to using the exclamation mark (!), we can use an if check statement):
+// const userInputElement = document.getElementById(
+//   'user-input'
+// ) as HTMLInputElement; // casting as HTMLInputElement defines outside the if statement
 
-moveAnimal({ type: 'bird', flyingSpeed: 400 });
-moveAnimal({ type: 'horse', runningSpeed: 800 });
+// if (userInputElement) {
+//   userInputElement.value = 'Hello there!';
+// }
+
+// or
+
+// const userInputElement = document.getElementById('user-input');
+
+// if (userInputElement) {
+//   (userInputElement as HTMLInputElement).value = 'Hello there!'; // casting as HTMLInputElement defines inside the if statement (userInputElement as HTMLInputElement))
+// }
 
 /*
   Notes:
-  - 
+  - Type casting helps we tell TypeScript that some value is of a specific type where TypeScript is 
+    not able to detect it on it's own, but we as a developer know that it will be the case.
+  - TypeScript does not dive into our HTML files and analyze them.
+  - The exclamation mark (!) allows us to tell TypeScript that the expression in front of it (e.g. document.getElementById('user-input')) will never yield null.
+    If we know as developer that this (userInputElement) will never return null, then we can use this exclamation mark.
+  - If we're not sure that this (userInputElement) will not return null, we can use an if check.
+  - Type Casting is called Type Assertions in the official Docs and it's explained here: https://www.typescriptlang.org/docs/handbook/2/everyday-types.html#type-assertions
 */
