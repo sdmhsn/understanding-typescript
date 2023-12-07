cf54c134e6c1c49462542f0feac44382c40569f1
Saddam Husein <saddambinhusein2@gmail.com>
2023-12-07 14:05:47 +0800

    Section 6: Advanced Types (87 Index properties)

--- 06 Advanced Types/87-index-properties/dist/app.js
+++ 06 Advanced Types/87-index-properties/dist/app.js
@@ -1,5 +1,15 @@
 "use strict";
+const errorBag = {
+    email: 'Not a valid email!',
+    username: 'Must start with a capital character!', // we can add multiple values here
+    /*
+      with the help of ErrorContainer, which gives us this extra flexibility that we don't need to know in advance
+      which property names we want to use and how many properties we need.
+    */
+};
 /*
   Notes:
-  -
+  - index types a feature that allows us to create objects which are more flexible regarding the properties they
+    might hold.
+  - flexible example: able to use it on any form we have in our webpage.
  */
--- 06 Advanced Types/87-index-properties/src/app.ts
+++ 06 Advanced Types/87-index-properties/src/app.ts
@@ -1,4 +1,20 @@
+interface ErrorContainer {
+  [prop: string]: string; // key/property => [prop: string], value => string;
+}
+
+const errorBag: ErrorContainer = {
+  email: 'Not a valid email!', // on value, it would not be okay if we instead assigned a number because we're saying every property needs a string value type. So we must use a string here.
+  username: 'Must start with a capital character!', // we can add multiple values here
+
+  /*
+    with the help of ErrorContainer, which gives us this extra flexibility that we don't need to know in advance
+    which property names we want to use and how many properties we need.
+  */
+};
+
 /*
   Notes:
-  -
+  - index types a feature that allows us to create objects which are more flexible regarding the properties they 
+    might hold.
+  - flexible example: able to use it on any form we have in our webpage.
  */
