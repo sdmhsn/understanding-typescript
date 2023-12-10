d8ffdd43a38cab70fa1c8362e453a1f3a6ca64b8
Saddam Husein <saddambinhusein2@gmail.com>
2023-12-10 13:27:17 +0800

    Section 6: Advanced Types (90 Nullish coalescing: project setup)

---
+++ 06 Advanced Types/90-nullish-coalescing/dist/app.js
@@ -0,0 +1,18 @@
+"use strict";
+const fetchedUserData = {
+    id: 'u1',
+    name: 'Saddam',
+    job: { title: 'CEO', description: 'My own company' },
+};
+// console.log(fetchedUserData.job.title);
+// console.log(fetchedUserData && fetchedUserData.job.title);
+// optional chaining operator:
+console.log(fetchedUserData?.job?.title); // this tells TypeScript does fetchedUserData exist?, if it does access job and hence here we can add that question mark and therefore only access title if job is defined.
+/*
+  Notes:
+  - optional chaining operator helps us safely access nested properties and nested objects in our object
+    data and if the thing in front of the question mark is undefined it will not access the thing thereafter
+    and therefore will not throw a runtime error but instead it will just not continue.
+  - so behind the scenes this is basically compiled to an if check which checks whether that exists before it
+    tries to access this.
+*/
