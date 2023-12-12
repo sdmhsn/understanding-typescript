b97237da447daaa8fd8d9d6a7d1cd3262e59f098
Saddam Husein <saddambinhusein2@gmail.com>
2023-12-12 12:04:21 +0800

    Section 7: Generics (96 Working with constraints)

--- 07 Generics/96-working-with-constraints/src/app.ts
+++ 07 Generics/96-working-with-constraints/src/app.ts
@@ -1,12 +1,24 @@
-/* Sample 1 */
-function merge<T, U>(objA: T, objB: U) {
-  return Object.assign(objA, objB); // typescript error: This type parameter might need an `extends {}` constraint.
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  /*
+    So here we could say extends object, and with that I'm saying, the T type can be any 
+    object with any structure, but it has to be an object. We can do the same for U.
+    And of course, we don't have to constraint all generic types. If we just want to 
+    constrain U, we can do that. In this example, it just makes sense to constraint both
+    because object assign wants two objects here, but we're really flexible there.
+  */
+  return Object.assign(objA, objB);
 }
 
-const mergedObj = merge({ name: 'saddam' }, { age: 30 });
+const mergedObj = merge({ name: 'saddam', hobbies: ['Sports'] }, { age: 30 });
+console.log(mergedObj);
 console.log(mergedObj.age);
 
 /*
   Notes:
-  - 
+  - we can restrict the types of T and U here to solve our generic types with type constraints.
+  - for generic types, we can set certain constraints regarding the types our generic types can be based on
+    and we do this with the "extends" keyword in the angled brackets after the type which we wanna constrain.
+  - and these constraints can be anything. we can refer to objects, to string, we could use our own type if 
+    we had it. we can also use union types if we want to, we're really flexible there, we can set any 
+    constraints we want.
 */
