d43944e5bbdeda245d2e59b83faec3a9aeca7953
Saddam Husein <saddambinhusein2@gmail.com>
2023-12-18 14:02:20 +0800

    Section 8: Decorators (105 A first class decorator: creating Person class)

--- 08 Decorators/105-a-first-class-decorator/dist/app.js
+++ 08 Decorators/105-a-first-class-decorator/dist/app.js
@@ -1,5 +1,27 @@
 "use strict";
+class Person {
+    constructor() {
+        this.name = 'Saddam';
+        console.log('Creating person object...');
+    }
+}
+const pers = new Person();
+console.log(pers); // Person { "name": "Saddam" }
+console.log(Person);
+/*
+output:
+  Creating person object...
+  Person {name: 'Saddam'}
+  class Person {
+    constructor() {
+        this.name = 'Saddam';
+        console.log('Creating person object...');
+    }
+  }
+*/
 /*
   Notes:
-  -
+  - Now decorators in general are all about classes, but this example is a decorator which we will add to the class as a whole.
+
+
 */
--- 08 Decorators/105-a-first-class-decorator/src/app.ts
+++ 08 Decorators/105-a-first-class-decorator/src/app.ts
@@ -1,4 +1,29 @@
+class Person {
+  name = 'Saddam';
+
+  constructor() {
+    console.log('Creating person object...');
+  }
+}
+
+const pers = new Person();
+console.log(pers); // Person { "name": "Saddam" }
+console.log(Person);
 /*
-  Notes:
-  - 
+output:
+  Creating person object...
+  Person {name: 'Saddam'}
+  class Person {
+    constructor() {
+        this.name = 'Saddam';
+        console.log('Creating person object...');
+    }
+  }
+*/
+
+/*
+  Notes: 
+  - Now decorators in general are all about classes, but this example is a decorator which we will add to the class as a whole.
+
+
 */
