eb0e067086269f5b2246b62e18f52aff5f1dd9ac
Saddam Husein <saddambinhusein2@gmail.com>
2023-12-26 18:51:25 +0800

    Section 8: Decorators (115 Validation with decorators - First steps: class Course and courseForm)

--- 08 Decorators/115-validation-with-decorators_(first-steps)/dist/app.js
+++ 08 Decorators/115-validation-with-decorators_(first-steps)/dist/app.js
@@ -1,5 +1,21 @@
 "use strict";
+class Course {
+    constructor(t, p) {
+        this.title = t;
+        this.price = p;
+    }
+}
+const courseForm = document.querySelector('form');
+courseForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const titleEl = document.getElementById('title');
+    const priceEl = document.getElementById('price');
+    const title = titleEl.value;
+    const price = +priceEl.value;
+    const createdCourse = new Course(title, price);
+    console.log(createdCourse);
+});
 /*
   Notes:
-  -
+  - Decorators for validation
 */
--- 08 Decorators/115-validation-with-decorators_(first-steps)/src/app.ts
+++ 08 Decorators/115-validation-with-decorators_(first-steps)/src/app.ts
@@ -1,4 +1,28 @@
+class Course {
+  title: string;
+  price: number;
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this.price = p;
+  }
+}
+
+const courseForm = document.querySelector('form')!;
+courseForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const titleEl = document.getElementById('title') as HTMLInputElement;
+  const priceEl = document.getElementById('price') as HTMLInputElement;
+
+  const title = titleEl.value;
+  const price = +priceEl.value;
+
+  const createdCourse = new Course(title, price);
+  console.log(createdCourse);
+});
+
 /*
   Notes: 
-  - 
+  - Decorators for validation
 */
