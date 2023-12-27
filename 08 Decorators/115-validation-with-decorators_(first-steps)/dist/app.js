ae62a1d5c5a623b7c65693353bfbbb2252f95298
Saddam Husein <saddambinhusein2@gmail.com>
2023-12-27 13:40:03 +0800

    Section 8: Decorators (115 Validation with decorators - First steps)

--- 08 Decorators/115-validation-with-decorators_(first-steps)/dist/app.js
+++ 08 Decorators/115-validation-with-decorators_(first-steps)/dist/app.js
@@ -1,10 +1,25 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function Required() { }
+function PositiveNumber() { }
+function validate(obj) { }
 class Course {
     constructor(t, p) {
         this.title = t;
         this.price = p;
     }
 }
+__decorate([
+    Required
+], Course.prototype, "title", void 0);
+__decorate([
+    PositiveNumber
+], Course.prototype, "price", void 0);
 const courseForm = document.querySelector('form');
 courseForm.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -13,6 +28,10 @@ courseForm.addEventListener('submit', (event) => {
     const title = titleEl.value;
     const price = +priceEl.value;
     const createdCourse = new Course(title, price);
+    if (!validate(createdCourse)) {
+        alert('Invalid input, please try again!');
+        return;
+    }
     console.log(createdCourse);
 });
 /*
--- 08 Decorators/115-validation-with-decorators_(first-steps)/src/app.ts
+++ 08 Decorators/115-validation-with-decorators_(first-steps)/src/app.ts
@@ -1,5 +1,13 @@
+function Required() {}
+
+function PositiveNumber() {}
+
+function validate(obj: object) {}
+
 class Course {
+  @Required
   title: string;
+  @PositiveNumber
   price: number;
 
   constructor(t: string, p: number) {
@@ -19,6 +27,11 @@ courseForm.addEventListener('submit', (event) => {
   const price = +priceEl.value;
 
   const createdCourse = new Course(title, price);
+
+  if (!validate(createdCourse)) {
+    alert('Invalid input, please try again!');
+    return;
+  }
   console.log(createdCourse);
 });
 
