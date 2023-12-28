cf8ecf52762aad517e4e1d041afc371a3921a5e1
Saddam Husein <saddambinhusein2@gmail.com>
2023-12-28 20:50:32 +0800

    Section 8: Decorators (116 Validation with decorators - Finished)

--- 08 Decorators/116-validation-with-decorators_(finished)/dist/app.js
+++ 08 Decorators/116-validation-with-decorators_(finished)/dist/app.js
@@ -5,9 +5,47 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function Required() { }
-function PositiveNumber() { }
-function validate(obj) { }
+const registeredValidators = {};
+function Required(target, propName) {
+    var _a, _b;
+    registeredValidators[target.constructor.name] = Object.assign(Object.assign({}, registeredValidators[target.constructor.name]), { 
+        // [propName]: ['required'], // bug
+        [propName]: [
+            ...((_b = (_a = registeredValidators[target.constructor.name]) === null || _a === void 0 ? void 0 : _a[propName]) !== null && _b !== void 0 ? _b : []),
+            'required',
+        ] });
+}
+function PositiveNumber(target, propName) {
+    var _a, _b;
+    registeredValidators[target.constructor.name] = Object.assign(Object.assign({}, registeredValidators[target.constructor.name]), { 
+        // [propName]: ['positive'], // bug
+        [propName]: [
+            ...((_b = (_a = registeredValidators[target.constructor.name]) === null || _a === void 0 ? void 0 : _a[propName]) !== null && _b !== void 0 ? _b : []),
+            'positive',
+        ] });
+}
+function validate(obj) {
+    // change obj: object type to obj: any to get this extra flexibility
+    // console.log(registeredValidators);
+    const objValidatorConfig = registeredValidators[obj.constructor.name];
+    if (!objValidatorConfig) {
+        return true;
+    }
+    let isValid = true;
+    for (const prop in objValidatorConfig) {
+        for (const validator of objValidatorConfig[prop]) {
+            switch (validator) {
+                case 'required':
+                    isValid = isValid && !!obj[prop]; // In JavaScript, the double exclamation operator converts an Object to Boolean. (source: https://www.logilax.com/javascript-double-exclamation-operator/#:~:text=In%20JavaScript%2C%20the%20double%20exclamation,!!)
+                    break;
+                case 'positive':
+                    isValid = isValid && obj[prop] > 0;
+                    break;
+            }
+        }
+    }
+    return isValid;
+}
 class Course {
     constructor(t, p) {
         this.title = t;
--- 08 Decorators/116-validation-with-decorators_(finished)/src/app.ts
+++ 08 Decorators/116-validation-with-decorators_(finished)/src/app.ts
@@ -1,8 +1,59 @@
-function Required() {}
+interface ValidatorConfig {
+  [property: string]: {
+    [ValidatableProp: string]: string[]; // ['required', 'positive']
+  };
+}
 
-function PositiveNumber() {}
+const registeredValidators: ValidatorConfig = {};
 
-function validate(obj: object) {}
+function Required(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    // [propName]: ['required'], // bug
+    [propName]: [
+      ...(registeredValidators[target.constructor.name]?.[propName] ?? []),
+      'required',
+    ], // fixing bug
+  };
+}
+
+function PositiveNumber(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    // [propName]: ['positive'], // bug
+    [propName]: [
+      ...(registeredValidators[target.constructor.name]?.[propName] ?? []),
+      'positive',
+    ], // fixing bug
+  };
+}
+
+function validate(obj: any) {
+  // change obj: object type to obj: any to get this extra flexibility
+  // console.log(registeredValidators);
+  const objValidatorConfig = registeredValidators[obj.constructor.name];
+
+  if (!objValidatorConfig) {
+    return true;
+  }
+
+  let isValid = true;
+
+  for (const prop in objValidatorConfig) {
+    for (const validator of objValidatorConfig[prop]) {
+      switch (validator) {
+        case 'required':
+          isValid = isValid && !!obj[prop]; // In JavaScript, the double exclamation operator converts an Object to Boolean. (source: https://www.logilax.com/javascript-double-exclamation-operator/#:~:text=In%20JavaScript%2C%20the%20double%20exclamation,!!)
+          break;
+        case 'positive':
+          isValid = isValid && obj[prop] > 0;
+          break;
+      }
+    }
+  }
+
+  return isValid;
+}
 
 class Course {
   @Required
@@ -32,6 +83,7 @@ courseForm.addEventListener('submit', (event) => {
     alert('Invalid input, please try again!');
     return;
   }
+
   console.log(createdCourse);
 });
 
