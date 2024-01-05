051d2e5421f53b9654ff669cb419c08af04b9155
Saddam Husein <saddambinhusein2@gmail.com>
2024-01-05 08:56:32 +0800

    Section 9: Practice Time! Build a Drag and Drop Project (123 Interacting with DOM elements)

--- 09 Practice Time - Build a Drag and Drop Project/123-interacting-with-dom-elements/dist/app.js
+++ 09 Practice Time - Build a Drag and Drop Project/123-interacting-with-dom-elements/dist/app.js
@@ -9,9 +9,35 @@ class ProjectInput {
         /* --- (start) render the element into dom --- */
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild;
+        this.element.id = 'user-input';
+        this.titleInputElement = this.element.querySelector('#title');
+        this.descriptionTextareaElement = this.element.querySelector('#description');
+        this.peopleInputElement = this.element.querySelector('#people');
+        // console.log(this.titleInputElement);
+        this.configure();
         this.attach();
         /* --- (end) render the element into dom --- */
     }
+    // binding using this:
+    submitHandler(event) {
+        event.preventDefault();
+        console.log(this.titleInputElement.value);
+        console.log(this.descriptionTextareaElement.value);
+        console.log(this.peopleInputElement.value);
+    }
+    configure() {
+        this.element.addEventListener('submit', this.submitHandler.bind(this));
+    }
+    /* binding using arrow function:
+    private submitHandler = (event: Event) => {
+      event.preventDefault();
+      console.log(this.titleInputElement.value);
+    };
+  
+    private configure() {
+      this.element.addEventListener('submit', this.submitHandler);
+    }
+    */
     attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
     }
@@ -25,11 +51,57 @@ function projectInput() {
   const hostElement = document.getElementById('app')! as HTMLDivElement;
   const importedNode = document.importNode(templateElement.content, true);
   const element = importedNode.firstElementChild as HTMLFormElement;
+  element.id = 'user-input';
 
+  const titleInputElement = element.querySelector(
+    '#title'
+  )! as HTMLInputElement;
+  const descriptionTextareaElement = element.querySelector(
+    '#description'
+  )! as HTMLTextAreaElement;
+  const peopleInputElement = element.querySelector(
+    '#people'
+  )! as HTMLInputElement;
+
+  const submitHandler = (event: Event) => {
+    event.preventDefault();
+    console.log(titleInputElement.value);
+    console.log(descriptionTextareaElement.value);
+    console.log(peopleInputElement.value);
+  };
+
+  element.addEventListener('submit', submitHandler);
   hostElement.insertAdjacentElement('afterbegin', element);
 }
 
 projectInput();
+*/
+/* Sample 3 (other experiment):
+const templateElement = document.getElementById(
+  'project-input'
+)! as HTMLTemplateElement;
+const hostElement = document.getElementById('app')! as HTMLDivElement;
+const importedNode = document.importNode(templateElement.content, true);
+const element = importedNode.firstElementChild as HTMLFormElement;
+const titleInputElement = element.querySelector('#title')! as HTMLInputElement;
+const descriptionTextareaElement = element.querySelector(
+  '#description'
+)! as HTMLTextAreaElement;
+const peopleInputElement = element.querySelector(
+  '#people'
+)! as HTMLInputElement;
+
+element.id = 'user-input';
+
+const submitHandler = (event: Event) => {
+  event.preventDefault();
+  console.log(titleInputElement.value);
+  console.log(descriptionTextareaElement.value);
+  console.log(peopleInputElement.value);
+};
+
+element.addEventListener('submit', submitHandler);
+hostElement.insertAdjacentElement('afterbegin', element);
 */
 /*  Notes:
     -
--- 09 Practice Time - Build a Drag and Drop Project/123-interacting-with-dom-elements/src/app.ts
+++ 09 Practice Time - Build a Drag and Drop Project/123-interacting-with-dom-elements/src/app.ts
@@ -3,6 +3,9 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionTextareaElement: HTMLTextAreaElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     /* --- (start) get the element by id --- */
@@ -18,9 +21,47 @@ class ProjectInput {
       true
     );
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input';
+
+    this.titleInputElement = this.element.querySelector(
+      '#title'
+    )! as HTMLInputElement;
+    this.descriptionTextareaElement = this.element.querySelector(
+      '#description'
+    )! as HTMLTextAreaElement;
+    this.peopleInputElement = this.element.querySelector(
+      '#people'
+    )! as HTMLInputElement;
+
+    // console.log(this.titleInputElement);
+
+    this.configure();
     this.attach();
     /* --- (end) render the element into dom --- */
   }
+
+  // binding using this:
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+    console.log(this.descriptionTextareaElement.value);
+    console.log(this.peopleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener('submit', this.submitHandler.bind(this));
+  }
+
+  /* binding using arrow function:
+  private submitHandler = (event: Event) => {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  };
+
+  private configure() {
+    this.element.addEventListener('submit', this.submitHandler);
+  }
+  */
 
   private attach() {
     this.hostElement.insertAdjacentElement('afterbegin', this.element);
@@ -37,11 +78,58 @@ function projectInput() {
   const hostElement = document.getElementById('app')! as HTMLDivElement;
   const importedNode = document.importNode(templateElement.content, true);
   const element = importedNode.firstElementChild as HTMLFormElement;
+  element.id = 'user-input';
 
+  const titleInputElement = element.querySelector(
+    '#title'
+  )! as HTMLInputElement;
+  const descriptionTextareaElement = element.querySelector(
+    '#description'
+  )! as HTMLTextAreaElement;
+  const peopleInputElement = element.querySelector(
+    '#people'
+  )! as HTMLInputElement;
+
+  const submitHandler = (event: Event) => {
+    event.preventDefault();
+    console.log(titleInputElement.value);
+    console.log(descriptionTextareaElement.value);
+    console.log(peopleInputElement.value);
+  };
+
+  element.addEventListener('submit', submitHandler);
   hostElement.insertAdjacentElement('afterbegin', element);
 }
 
 projectInput();
+*/
+
+/* Sample 3 (other experiment): 
+const templateElement = document.getElementById(
+  'project-input'
+)! as HTMLTemplateElement;
+const hostElement = document.getElementById('app')! as HTMLDivElement;
+const importedNode = document.importNode(templateElement.content, true);
+const element = importedNode.firstElementChild as HTMLFormElement;
+const titleInputElement = element.querySelector('#title')! as HTMLInputElement;
+const descriptionTextareaElement = element.querySelector(
+  '#description'
+)! as HTMLTextAreaElement;
+const peopleInputElement = element.querySelector(
+  '#people'
+)! as HTMLInputElement;
+
+element.id = 'user-input';
+
+const submitHandler = (event: Event) => {
+  event.preventDefault();
+  console.log(titleInputElement.value);
+  console.log(descriptionTextareaElement.value);
+  console.log(peopleInputElement.value);
+};
+
+element.addEventListener('submit', submitHandler);
+hostElement.insertAdjacentElement('afterbegin', element);
 */
 
 /*  Notes:
