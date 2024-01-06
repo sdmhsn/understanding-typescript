8418554d9c5f4cf6ddfaaa8ed91b30b960e6dc9e
Saddam Husein <saddambinhusein2@gmail.com>
2024-01-06 14:31:44 +0800

    Section 9: Practice Time! Build a Drag and Drop Project (125 Fetching user input)

--- 09 Practice Time - Build a Drag and Drop Project/125-fetching-user-input/src/app.ts
+++ 09 Practice Time - Build a Drag and Drop Project/125-fetching-user-input/src/app.ts
@@ -60,13 +60,52 @@ class ProjectInput {
     /* --- (end) render the element into dom --- */
   }
 
+  private gatherUserInput(): [string, string, number] | void {
+    // [string, string, number] --> tuple type
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionTextareaElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    if (
+      enteredTitle.trim().length === 0 ||
+      enteredDescription.trim().length === 0 ||
+      enteredPeople.trim().length === 0
+    ) {
+      alert('Invalid input, please try again!');
+      return; // we should add union void (| void) on the gatherUserInput() function, to avoid "Type 'undefined' is not assignable to type '[string, string, number]'" error
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeople]; // +enteredPeople: will convert it into a number
+    }
+  }
+
+  /* to clear all the inputs */
+  private clearInputs() {
+    this.titleInputElement.value = '';
+    this.descriptionTextareaElement.value = '';
+    this.peopleInputElement.value = '';
+  }
+
   /* binding using decorator: */
   @AutoBind // note: when this AutoBind access error (Decorator function return type 'PropertyDescriptor' is not assignable to type 'void | ((event: Event) => void)'), just enable the "experimentalDecorators" in tsconfig.json to true
   submitHandler(event: Event) {
     event.preventDefault();
-    console.log(this.titleInputElement.value);
-    console.log(this.descriptionTextareaElement.value);
-    console.log(this.peopleInputElement.value);
+    const userInput = this.gatherUserInput();
+    // console.log(userInput); // e.g. ['Title', 'Sample Description', 3]
+
+    if (Array.isArray(userInput)) {
+      /*
+        Note: 
+        - tuple is not a JavaScript type, its only exists in TypeScript. We can't check the if (typeof userInput === 'tuple') like this.
+        - there is also no if (userInput instanceof Tuple)
+        -  tuples in the end are just arrays.
+        - all we need to check here is whether it's an array, and for that, we can use the array object in JavaScript, and there the isArray method. (e.g. Array.isArray(userInput))
+        - isArray() is a method built into Vanilla JavaScript which allows us to check whether this here is an array.
+        */
+      const [title, desc, people] = userInput;
+      console.log(title, desc, people);
+
+      this.clearInputs(); // to clear all inputs
+    }
   }
 
   private configure() {
